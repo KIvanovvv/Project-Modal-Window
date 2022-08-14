c335ae7f6b364afb1d65f8869c348168cfad1190
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-14 17:57:29 +0100

    Add code
    Adding feature of closing modal window with the "Escape" key.

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const closeModal = function () {
   overlay.classList.add(`hidden`);
 };
 const openModal = function () {
-  console.log(`Btn clicked`);
   modal.classList.remove(`hidden`);
   overlay.classList.remove(`hidden`);
 };
@@ -18,3 +17,8 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener(`click`, closeModal);
 overlay.addEventListener(`click`, closeModal);
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
